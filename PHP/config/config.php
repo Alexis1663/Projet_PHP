f37942002dfe3751b24549974e410ef0c184e612
LouBRODA <115875561+LouBRODA@users.noreply.github.com>
2022-11-18 08:46:53 +0100

    new rep PHP

---
+++ PHP/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+//gen
+$rep=__DIR__.'/../';
+
+
+//BDD
+
+$user = "lobroda";
+$password = "achanger";
+$dns = "mysql:host=londres.uca.local;dbname=dblobroda";
+
+
+//Vues
+
+$vues['erreur']='vues/erreur.php';
+$vues['accueil']='vues/accueil.php';
+
+
+?>
---
+++ PHP/vue/accueil.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/accueil.css">
+    
+    <title>COURNONTTNEWS</title>
+</head>
+<body>
+
+    <!-- Début de l'entête de page -->
+
+    <header>
+
+        <nav>
+
+            <div class="logo">
+                <a href="accueil.html"><img src="../img/logo_cournontt.png" alt="logo_cournontt"></a>
+            </div>
+
+			<div class="titre">
+				<h1>COURNON TT NEWS</h1>
+				<a href="inscription.php">Rechercher un article...</a>
+				<p> ARTICLES POSTES</p>
+			</div>
+
+            <div class="log">
+                <a href="inscription.php">S'inscrire</a>
+                <a href="connexion.php">Se connecter</a>
+            </div>
+
+        </nav>
+
+    </header>
+
+    <!-- Début du corp de code -->
+
+    <section>
+
+        <div class="container">
+
+            <div class="texte">
+
+                <h1>Votre palais,<br>Notre priorité</h1>
+                <h3>Le fromage autrement par FromageOpédie</h3>
+
+            </div>
+            
+        </div>
+
+        <div class="container_explication">
+
+            <div class="expl1">
+                <h1>Qui sommes nous ?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi itaque neque maiores nesciunt ab! Corrupti, sapiente. Delectus facere voluptatibus, inventore rerum quam, esse quae dolorem libero repudiandae in fugiat illum?</p>
+            </div>
+
+            <div class="expl2">
+                <h1>Pourquoi nous rejoindre ?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus vero labore sapiente non eaque blanditiis aut alias, ut repellendus nobis voluptatibus quia sit nesciunt itaque reprehenderit eos praesentium assumenda a.</p>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- Début du pied de page -->
+
+    <footer>
+
+		<div class="footer">
+			<p>CL COURNON TT</p>
+			<p>exemple@exemple.fr</p>
+		</div>
+
+    </footer>
+
+</body>
+</html>
+
