18050ce87070b0e0c9092a9d2a5260162000c2bb
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2022-11-30 08:04:37 +0100

    correction erreur require

---
+++ mvc2_autoloadsimple/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+//gen
+$rep = __DIR__ . '/../';
+
+// liste des modules à inclure
+
+//$dConfig['includes']= array('controleur/Validation.php');
+
+
+
+//BD
+
+$base = "projet_php";
+$login = "root";
+$mdp = "Root-linux63";
+
+//Vues
+
+$vues['erreur'] = 'vues/erreur.php';
+$vues['vuephp1'] = 'vues/vuephp1.php';
