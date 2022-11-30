ed5e51b536e04e5c27be569af7a32ab8948f96a2
LouBRODA <115875561+LouBRODA@users.noreply.github.com>
2022-11-30 09:53:48 +0100

    Add files via upload

---
+++ PHP/modele/admin.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once ('user.php');
+
+class Admin extends User
+{
+	private string $pseudo;
+	private string $nom;
+	private string $prenom;
+	private string $motDePasse;	
+	private int $nmbCommentairesEcrits;
+	private int $nmbArticlesEcrits;
+
+	public function __construct(int $nmbArticlesEcrits=0){
+		parent::__construct($this->pseudo, $this->nom, $this->prenom, $this->motDePasse, $this->nmbCommentairesEcrits=0);
+		$this->nmbArticlesEcrits=$nmbArticlesEcrits;
+	}
+
+	public function __getNmbArticlesEcrits():int{
+		return $this->nmbArticlesEcrits;
+	}	
+
+	public function __setNmbCommentairesEcrits($nmb):int{
+		return $this->nmbCommentairesEcrits=$nmb;
+	}
+
+}
+
+?>
