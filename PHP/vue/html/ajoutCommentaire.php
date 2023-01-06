d414e3e58282728965f02b49af7f3fbd50b34b6b
Alexis CARREAU <92861778+Alexis1663@users.noreply.github.com>
2023-01-06 07:21:00 +0100

    Add files via upload

---
+++ PHP/vue/html/ajoutCommentaire.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COURNONTT</title>
+    <link rel="stylesheet" href="vue/css/header_footer.css">
+    <link rel="stylesheet" href="vue/css/ajoutCommentaire.css">
+</head>
+
+<body>
+
+    <!-- Début de l'entête de page -->
+
+    <header>
+
+        <nav>
+
+            <div class="logo">
+                <a href="index.php?page=accueil"><img src="./vue/img/logo_cournontt.png" alt="logo_cournontt"></a>
+            </div>
+
+            <div class="titre">
+                <h1>COURNON TT NEWS</h1>
+            </div>
+
+            <div class="log">
+                <?php if (isset($_SESSION['login'])) { ?>
+                    <a href="index.php?page=logout"><button class="button">Deconnexion</button></a>
+                <?php } else { ?>
+                    <a href="index.php?page=connexion"><button class="button">Connexion Admin</button></a>
+                <?php } ?>
+            </div>
+
+        </nav>
+
+    </header>
+
+    <!-- Début du corp de code -->
+
+    <section>
+        <h1>Donnez votre avis !!!</h1>
+        <form action="index.php?page=ajouterCommentaire" method="POST">
+            <div class="container">
+                <label for="pseudo">Pseudo</label>
+                <input name="pseudo" type="text">
+
+
+
+                <label for="titre">Titre du commentaire</label>
+                <input name="titre" type="text">
+
+
+
+                <label for="contenu">Contenu</label>
+                <textarea name="contenu" type="text"></textarea>
+
+
+                <?php
+                foreach ($dVueErreur as $row) {
+                    echo '<p style="color: red;">' . $row . '</p>';
+                }
+                ?>
+
+                <input type="hidden" name="titreArticle" value="<?= $titreArticle ?>">
+                <input type="hidden" name="dateArticle" value="<?= $dateArticle ?>">
+                <input type="submit" name="formulaire_ajout_commentaire" value="commenter">
+
+            </div>
+        </form>
+
+    </section>
+
+    <!-- Début du pied de page -->
+
+    <footer>
+
+        <div class="footer">
+            <p>CL COURNON TT</p>
+            <p>exemple@exemple.fr</p>
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
